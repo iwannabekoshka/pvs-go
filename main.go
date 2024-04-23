5691d1b225f5f0452a3b8d84ef6f2ceeed03a5da
iwannabekoshka <pokusaeff.daniil@gmail.com>
2024-04-23 17:54:02 +0300

    feat: добавил страницу одной статьи
    Плюсом добавил темплейт фильтр strToHTML

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,10 +14,21 @@ type articleType struct {
 	Content     string
 }
 
+var articles = []articleType{
+	{Id: "1", Title: "Articlus 1", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsum</b></p>"},
+	{Id: "2", Title: "AArticlus 2", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsumpipsum</b></p>"},
+	{Id: "3", Title: "Arrticlus 3", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsum</b>pipsum</p>"},
+	{Id: "4", Title: "Artticlus 4", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsum</b>pipsumpipsum</p>"},
+}
+
 func main() {
 	ginEngine := gin.Default()
 
 	ginEngine.Static("/public", "./public")
+
+	ginEngine.SetFuncMap(template.FuncMap{
+		"strToHTML": strToHTML,
+	})
 	ginEngine.LoadHTMLGlob("templates/**/*")
 
 	ginEngine.GET("/", func(context *gin.Context) {
@@ -38,14 +50,31 @@ func main() {
 			"meta": gin.H{
 				"title": "Blogus Listus",
 			},
-			"articles": []articleType{
-				{Id: "1", Title: "Articlus 1", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsum<b></p>"},
-				{Id: "2", Title: "AArticlus 2", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsum<b></p>"},
-				{Id: "3", Title: "Arrticlus 3", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsum<b></p>"},
-				{Id: "4", Title: "Artticlus 4", Description: "Coolus articlus aboutus somethingus goose", Content: "<p>lorem <b>pipsum<b></p>"},
+			"articles": articles,
+		})
+	})
+	ginEngine.GET("/blog/:id", func(context *gin.Context) {
+		var articleId = context.Param("id")
+		var article articleType
+
+		for _, _article := range articles {
+			if _article.Id == articleId {
+				article = _article
+				break
+			}
+		}
+
+		context.HTML(http.StatusOK, "content.html", gin.H{
+			"meta": gin.H{
+				"title": article.Title,
 			},
+			"content": article,
 		})
 	})
 
 	ginEngine.Run(":9000")
+}
+
+func strToHTML(str string) template.HTML {
+	return template.HTML(str)
 }
